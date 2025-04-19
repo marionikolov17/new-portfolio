f471f1a49e03406fcb912a01f6b45a9e19541b62
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2025-04-19 15:08:51 +0300

    Fix: home: floating social media icons were not clickable

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -5,13 +5,17 @@ export default function Navigation() {
   const { isNavOpen } = useNavigation();
 
   return (
-    <div className="fixed top-0 left-0 h-screen w-64 z-50">
-      <div className="absolute top-0 left-0 mx-8 my-0 z-40">
+    <>
+      <div className="fixed top-0 left-0 mx-8 my-0 z-50">
         <BurgerMenuIcon />
       </div>
       <div
-        className={`w-full h-full bg-background-300 z-30 transition-all duration-300 ease-in-out ${isNavOpen ? 'translate-x-0' : '-translate-x-full'}`}
-      ></div>
-    </div>
+        className={`fixed top-0 left-0 h-screen w-64 z-40 ${isNavOpen ? 'translate-x-0' : '-translate-x-full'}`}
+      >
+        <div
+          className={`w-full h-full bg-background-300 z-30 transition-all duration-300 ease-in-out`}
+        ></div>
+      </div>
+    </>
   );
 }
