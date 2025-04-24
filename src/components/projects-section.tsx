5541200d7fb373ef6248d8fc71bec5fec0121d63
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2025-04-24 22:13:09 +0300

    Improve: home: projects-section: add responsiveness

--- src/components/projects-section.tsx
+++ src/components/projects-section.tsx
@@ -7,7 +7,7 @@ import { projects } from '@/constants/projects';
 export default function ProjectsSection() {
   return (
     <div className="w-full min-h-screen my-8 flex items-center justify-center">
-      <div className="w-1/2 flex flex-col gap-y-4 overflow-x-hidden">
+      <div className="w-full px-8 lg:px-0 lg:w-[75%] xl:w-[65%] 2xl:w-1/2 flex flex-col gap-y-4 overflow-x-hidden">
         <SectionDelimiter title="Projects" number="03" />
         {projects.map((project, index) => (
           <ProjectCard key={index} index={index} {...project} />
@@ -41,24 +41,24 @@ function ProjectCard({
 
   return (
     <div
-      className={`w-full min-h-[350px] mb-20 flex overflow-x-hidden ${shouldInvert ? 'flex-row-reverse' : ''}`}
+      className={`w-full min-h-[350px] gap-4 lg:gap-0 mb-10 sm:mb-20 flex flex-col sm:flex-row overflow-x-hidden ${shouldInvert ? 'flex-col-reverse sm:flex-row-reverse' : ''}`}
     >
-      <div className="w-[55%] grow shrink-0 overflow-hidden rounded-lg h-[350px]">
+      <div className="w-full sm:w-1/2 lg:w-[55%] grow shrink-0 overflow-hidden rounded-lg h-[300px] sm:h-[350px]">
         <div className="w-full h-full bg-primary-text-500"></div>
       </div>
       <div
-        className={`w-[45%] grow shrink-0 ${shouldInvert ? 'text-left' : 'text-right'} relative h-max flex flex-col justify-center items-end ${shouldInvert ? 'items-start' : ''}`}
+        className={`w-full sm:w-[45%] grow shrink-0 ${shouldInvert ? 'text-left' : 'text-right'} relative h-max flex flex-col justify-center items-end ${shouldInvert ? 'items-start' : ''}`}
       >
         <h4 className="text-blue-500 text-base">{type}</h4>
         <h1 className="text-3xl font-bold text-primary-text-500 mt-2">
           {title}
         </h1>
-        <div className="mt-8 relative p-4 h-max">
+        <div className="mt-8 relative px-0 py-4 lg:px-4 h-max">
           <p className="text-sm text-primary-text-500 z-10 relative">
             {description}
           </p>
           <div
-            className={`absolute z-0 top-0 w-[500px] h-full bg-background-200 rounded-lg ${shouldInvert ? 'left-0' : 'right-0'}`}
+            className={`absolute hidden lg:flex z-0 top-0 w-[400px] lg:w-[500px] h-full bg-background-200 rounded-lg ${shouldInvert ? 'left-0' : 'right-0'}`}
           ></div>
         </div>
         <div
