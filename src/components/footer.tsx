8882be5617d2f64df8eb3f02d053617f950acd85
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2025-04-24 22:22:09 +0300

    Improve: footer: add responsiveness

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="w-full py-4 flex justify-center items-center">
-      <p className="text-secondary-text-500 text-base">
+      <p className="text-secondary-text-500 text-sm sm:text-base">
         © 2025 Mario Nikolov. All rights reserved.
       </p>
     </footer>
