ec7248bc022293a83039d75d6d0f404c76235b0f
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2025-04-26 11:32:26 +0300

    Add: metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,45 @@
 import LayoutHome from '@/components/layout-home';
 import { NavigationProvider } from '@/context/navigation-context';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Mario Nikolov Developer',
+  description: 'Personal website of Mario Nikolov, a software developer.',
+  creator: 'Mario Nikolov',
+  keywords: [
+    'Mario Nikolov',
+    'Developer',
+    'Software Engineer',
+    'Full Stack Developer',
+    'Frontend Developer',
+    'Backend Developer',
+    'React Developer',
+    'Node.js Developer',
+    'TypeScript Developer',
+    'JavaScript Developer',
+  ],
+  robots: {
+    index: true,
+    follow: true,
+  },
+  icons: {
+    icon: '/icons/favicon.ico',
+  },
+  openGraph: {
+    title: 'Mario Nikolov Developer',
+    description: 'Personal website of Mario Nikolov, a software developer.',
+    url: 'https://www.marionikolovdev.com',
+    siteName: 'Mario Nikolov Developer',
+    images: [
+      {
+        url: '/images/opengraph.png',
+        width: 1200,
+        height: 630,
+        alt: 'Mario Nikolov Developer',
+      },
+    ],
+  },
+};
 
 export default function Home() {
   return (
